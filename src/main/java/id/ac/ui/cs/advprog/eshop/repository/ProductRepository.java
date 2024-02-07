e06e2c9896949b688f001ba991945a54545f5133
arjunajaa <arju.naja@ui.ac.id>
2024-02-07 18:43:04 +0700

    add edit html

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -18,7 +18,7 @@ public class ProductRepository {
     public Iterator<Product> findAll(){
         return productData.iterator();
     }
-    private Product findById(String productId) {
+    public Product findById(String productId) {
         for (Product product : productData) {
             if (product.getProductId().equals(productId)) {
                 return product;

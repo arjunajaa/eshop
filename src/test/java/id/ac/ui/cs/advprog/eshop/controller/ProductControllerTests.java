38d2d135abfcd18a46dd6a322fea15a1471f97e8
arjunajaa <arju.naja@ui.ac.id>
2024-02-14 20:19:54 +0700

    fix sonarcloud: modifier

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTests.java
@@ -15,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class ProductControllerTests {
+class ProductControllerTests {
     @Mock
     ProductService productService;
     @Mock

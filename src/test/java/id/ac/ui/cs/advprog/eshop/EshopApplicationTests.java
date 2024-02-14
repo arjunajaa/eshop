4a62185fe6dc4a82f2975e3a55d936596d064541
arjunajaa <arju.naja@ui.ac.id>
2024-02-14 20:16:05 +0700

    fix sonarcloud issue: modifier

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -3,9 +3,9 @@ package id.ac.ui.cs.advprog.eshop;
 import org.junit.jupiter.api.Test;
 
 // Test class added ONLY to cover main() invocation not covered by application tests.
-public class EshopApplicationTests {
+class EshopApplicationTests {
     @Test
-    public void main() {
+     void main() {
         EshopApplication.main(new String[] {});
     }
 }

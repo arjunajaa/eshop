de092b89d4293441044df2b220884845e511d58c
arjunajaa <arju.naja@ui.ac.id>
2024-03-06 16:50:15 +0700

    [GREEN] Implement Order model.

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -41,9 +41,9 @@ public class OrderTest {
                 this.products, 1708560000L, "Safira Sudrajat");
 
         assertSame(this.products, order.getProducts());
-        assertEquals(2,order.getProducts());
+        assertEquals(2,order.getProducts().size());
         assertEquals("Sampo Cap Bambang",order.getProducts().get(0).getProductName());
-        assertEquals("Sampo Cap Usep",order.getProducts().get(1).getProductName());
+        assertEquals("Sabun Cap Usep",order.getProducts().get(1).getProductName());
 
         assertEquals("13652556-012a-4c07-b546-54eb1396d79b",order.getId());
         assertEquals(1708560000L,order.getOrderTime());
@@ -54,7 +54,7 @@ public class OrderTest {
     @Test
     void testCreateOrderSuccessStatus(){
         Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
-                this.products, 1708560000L, "Safira Sudrajat");
+                this.products, 1708560000L, "Safira Sudrajat","SUCCESS");
         assertEquals("SUCCESS", order.getStatus());
     }
 

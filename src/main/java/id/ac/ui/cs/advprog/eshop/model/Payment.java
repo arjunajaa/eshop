a3c0385c1295bdd78ed7efafaa78ae6b961d7f87
arjunajaa <arju.naja@ui.ac.id>
2024-03-06 18:31:53 +0700

    [GREEN] Implement Order model.

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -12,14 +12,30 @@ public class Payment {
     Map<String, String> paymentData;
 
     public Payment(String id, String method, Map<String, String> paymentData) {
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
+        this.status = "REJECTED";
     }
 
     public Payment(String id, String method, Map<String, String> paymentData, String status) {
+        this(id, method, paymentData);
+        this.setStatus(status);
     }
 
     public void setStatus(String status) {
+        if (status.equals("SUCCESS") || status.equals("REJECTED")) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
     public void setPaymentData(Map<String, String> paymentData) {
+        if (paymentData.isEmpty()) {
+            throw new IllegalArgumentException();
+        } else {
+            this.paymentData = paymentData;
+        }
     }
 }
